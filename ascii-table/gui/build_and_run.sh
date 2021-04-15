666724582fea4636040634f2836df2fabe966eba
w1jtoo <wanadooht@gmail.com>
2021-04-15 19:53:10 +0500

    debug arg added to run script

--- ascii-table/gui/build_and_run.sh
+++ ascii-table/gui/build_and_run.sh
@@ -15,12 +15,23 @@ fi
 
 nasm src/main.asm -o $path_to_main
 
+
 touch $path_to_temp_file
 echo "@echo off"                    >> $path_to_temp_file
 echo "cls"                          >> $path_to_temp_file
-echo $main                          >> $path_to_temp_file
-echo "echo."                        >> $path_to_temp_file
-echo "pause Press any key to exit"  >> $path_to_temp_file
+if [ "$1" = "debug" ];
+    then
+        td="TD.EXE"
+        cp ../../deps/$td $bin
+        echo "${td} $main"          >> $path_to_temp_file
+    else
+        echo $main                  >> $path_to_temp_file
+fi
+
+if [ -d $1 ]; then
+    echo "echo."                        >> $path_to_temp_file
+    echo "pause Press any key to exit"  >> $path_to_temp_file
+fi
 echo "exitemu"                      >> $path_to_temp_file
 
 dosemu $path_to_temp_file
